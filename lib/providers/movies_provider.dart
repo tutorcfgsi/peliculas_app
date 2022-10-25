bb8f7bc7da9b10131caf8986d5e084afd9eb6c29
Raul Reyes <tutorcfgsi@hotmail.com>
2022-10-25 18:11:28 +0200

    Mapeo de datos

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MoviesProvider extends ChangeNotifier {
   final String _apiKey = '0725904af9d713d86588bc91e7a508da';
@@ -20,6 +21,7 @@ class MoviesProvider extends ChangeNotifier {
 
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
-    print(response.body);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    print(decodeData['dates']);
   }
 }
